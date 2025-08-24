c095210d4a51472d9823fedd848b2e28bb2a8764
Binyamin Aron Green <binyamingreen@protonmail.com>
2025-08-24 17:39:23 -0400

    Add biome formatter+linter

--- index.js
+++ index.js
@@ -8,61 +8,58 @@
  * @param {string?} options.class add a class (preserves existing)
  */
 
-export default function (md, options={}) {
+export default function (md, options = {}) {
+	// Finds external links
+	let regex = /^https?:\/\//;
 
-    // Finds external links
-    let regex = /^https?:\/\//;
+	if (!!options.domain) {
+		// Sanitize domain - remove the protocol, if existing
+		const domain = options.domain.replace(/^https?:\/\//, '');
 
-    if(!!options.domain) {
-        // Sanitize domain - remove the protocol, if existing
-        const domain = options.domain.replace(/^https?:\/\//, "");
+		regex = RegExp(`^https?:\/\/(?!${domain}|localhost)`);
+	}
 
-        regex = RegExp(`^https?:\/\/(?!${domain}|localhost)`);
-    }
+	/**
+	 * @type {import("markdown-it/lib/parser_core").RuleCore}
+	 */
+	function link_external({ tokens }) {
+		// Iterate through tokens, looking for links
+		for (let t of tokens) {
+			// There are no tokens, or we got an empty one (skip)
+			if (!t) continue;
 
+			// It's a block, but any links will be inline (skip)
+			if (t.type !== 'inline') continue;
 
-    /**
-     * @type {import("markdown-it/lib/parser_core").RuleCore}
-     */
-    function link_external({ tokens }) {
-        // Iterate through tokens, looking for links
-        for(let t of tokens) {
-            // There are no tokens, or we got an empty one (skip)
-            if(!t) continue;
+			// It's inline, but there are no child tokens (skip)
+			if (!t.children) continue;
 
-            // It's a block, but any links will be inline (skip)
-            if(t.type !== 'inline') continue;
+			// There's children, but no links
+			if (t.children.filter((c) => c.type === 'link_open').length === 0) {
+				continue;
+			}
 
-            // It's inline, but there are no child tokens (skip)
-            if(!t.children) continue;
+			for (let c of t.children) {
+				// It's not a link (skip)
+				if (c.type !== 'link_open') continue;
 
-            // There's children, but no links
-            if(t.children.filter(c => c.type === "link_open").length === 0) {
-                continue;
-            }
+				let href = c.attrGet('href');
 
-            for(let c of t.children) {
+				// It's a link tag, but the url is missing (skip)
+				if (!href) continue;
 
-                // It's not a link (skip)
-                if(c.type !== "link_open") continue;
+				// It's a link, but it's internal (skip)
+				if (!regex.test(href)) continue;
 
-                let href = c.attrGet("href");
+				// We can start messing around now!
 
-                // It's a link tag, but the url is missing (skip)
-                if(!href) continue;
+				// Note: `attrJoin` preserves existing values
+				c.attrJoin('rel', 'noopener noreferrer');
+				c.attrSet('target', '_blank');
+				if (!!options.class) c.attrJoin('class', options.class);
+			}
+		}
+	}
 
-                // It's a link, but it's internal (skip)
-                if(!regex.test(href)) continue;
-
-                // We can start messing around now!
-
-                // Note: `attrJoin` preserves existing values
-                c.attrJoin("rel", "noopener noreferrer")
-                c.attrSet("target", "_blank")
-                if(!!options.class) c.attrJoin("class", options.class)
-            }
-        }
-    }
-
-    md.core.ruler.push("link_external", link_external);
-};
+	md.core.ruler.push('link_external', link_external);
+}
